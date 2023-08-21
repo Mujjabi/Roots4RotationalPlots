be62a012980f64e02ff963d3a1259658f5db3d74
Christopher <mujjabi2@illinois.edu>
2023-08-21 13:54:17 -0500

    Data cleaning

--- Data_Analysis.R
+++ Data_Analysis.R
@@ -1,7 +1,29 @@
-#This is a script for analysis root data from the rotational plots. 
+# This is a script for analysis root data from the rotational plots. 
+
+## Importing raw data
+Roots <- read.csv("Roots2023_Clean.csv", header = TRUE)
+
+#Change variables to numeric or factors
+Roots$BarCode <- as.factor(Roots$BarCode)
+Roots$View <- as.factor(Roots$View)
+Roots$FDT <- as.numeric(Roots$FDT)
+Roots$FDS <- as.numeric(Roots$FDS)
+Roots$LDA <- as.numeric(Roots$LDA)
+Roots$RTA <- as.numeric(Roots$RTA)
+Roots$SD <- as.numeric(Roots$SD)
+Roots$RTW <- as.numeric(Roots$RTW)
 
 
-Roots <- read.csv("data", header = TRUE)
+## Estimating means of roots architectural variables from the 4 views
 
-#Change variables to characters or factors
+
+
+
+
+
+## Combining root data with fieldbook design variables using root ID
+
+
+
+
 
